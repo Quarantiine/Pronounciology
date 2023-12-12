aad829469c00c779813783c006041af46db55a12
Daniel Ward <02danwar.buss@gmail.com>
2023-12-12 11:56:44 -0600

    added feature to the definition section

--- src/components/InputTextDefinition.jsx
+++ src/components/InputTextDefinition.jsx
@@ -1,17 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { createPortal } from "react-dom";
+import Image from "next/image";
 
 export default function InputTextDefinition({
 	textDefinition,
 	loadingDefinition,
-	inputDefinition,
 }) {
+	const [copied, setCopied] = useState(false);
+	const [definition, setDefinition] = useState("");
+	const timeoutRef = useRef();
+
 	const handleCopyText = async (definition) => {
+		clearTimeout(timeoutRef.current);
+
+		setCopied(true);
+		setDefinition(definition);
+
+		timeoutRef.current = setTimeout(() => {
+			setCopied(false);
+			setDefinition("");
+		}, 5000);
 		await navigator.clipboard.writeText(definition);
 	};
 
 	return (
 		<>
 			<div className="definition-container-overflow w-full max-h-[200px] h-fit border-2 bg-white rounded-3xl p-5 flex flex-col justify-start items-start overflow-x-hidden overflow-y-scroll gap-2 relative">
+				{copied &&
+					createPortal(
+						<>
+							<div className="flex justify-between items-center gap-2 fixed top-5 left-1/2 -translate-x-1/2 w-96 h-fit bg-white rounded-3xl border px-5 py-3 border-green-500 ">
+								<div className="flex flex-col gap-1">
+									<p className="text-sm font-bold">Copied Definition:</p>
+									<p className="text-sm">{definition}</p>
+								</div>
+								<Image
+									src={"/icons/copied.svg"}
+									alt="info"
+									width={25}
+									height={25}
+								/>
+							</div>
+						</>,
+						document.body
+					)}
 				{loadingDefinition && (
 					<div className="absolute top-0 left-0 w-full h-full bg-[rgba(255,255,255,0.9)] flex justify-center items-center">
 						<div className="min-w-[32px] max-w-[32px] min-h-[32px] max-h-[32px] border-4 border-t-transparent animate-spin border-blue-500 rounded-full" />
