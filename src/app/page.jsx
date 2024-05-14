5f3f2a1a6f1d81adc97d793241e60526c4b4c5e5
Daniel Ward <50274655+Quarantiine@users.noreply.github.com>
2024-05-14 02:14:11 -0500

    sm change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,9 +3,12 @@ import PronounceContainer from "@/components/PronounceContainer";
 export default function Home() {
 	return (
 		<>
-			<p className="text-[10px] text-gray-400 mx-auto absolute top-3 left-1/2 -translate-x-1/2">
-				By: Daniel Ward
-			</p>
+			<div className="flex flex-col justify-center items-center gap-1">
+				<p className="text-[10px] text-gray-400 mx-auto absolute top-3 left-1/2 -translate-x-1/2">
+					By: Daniel Ward
+				</p>
+				<p>Some languages are unresponsive to certain computers</p>
+			</div>
 
 			<main
 				className={`flex flex-col gap-4 justify-start items-center absolute top-1/2 -translate-y-1/2 left-0 w-full h-full sm:h-fit pb-5 pt-10 px-5 sm:px-10`}
